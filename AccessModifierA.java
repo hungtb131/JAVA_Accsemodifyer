d5c42e04ccd42f3577bb120e786e7e20d0132e59
hungtb131 <66382816+hungtb131@users.noreply.github.com>
2020-07-10 08:06:53 +0700

    add bt

---
+++ AccessModifierA.java
@@ -0,0 +1,7 @@
+package AcessModifier;
+
+ public class AccessModifierA {
+     protected void msg() {
+         System.out.println("Hello");
+     }
+ }
---
+++ AccessModifierB.java
@@ -0,0 +1,11 @@
+package AcessModifier;
+
+public class AccessModifierB {
+    public static void main(String[] args) {
+        AccessModifierB obj = new AccessModifierB();
+        obj.msg("");
+    }
+
+    private void msg(String s) {
+    }
+}
